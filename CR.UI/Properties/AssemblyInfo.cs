deaccf83c741ba3332b2a0fc67eee74cd47179b4
Angel Perez Severino <angel.perez011@gmail.com>
2021-02-09 20:33:43 -0400

    Remove install project

--- CR.UI/Properties/AssemblyInfo.cs
+++ CR.UI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CR.UI")]
+[assembly: AssemblyTitle("Cash Register")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CR.UI")]
+[assembly: AssemblyProduct("Cash Register")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
